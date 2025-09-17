c23c205fc42291c436264b01b2e91389e38d1202
Animes <animesbarman101@gmail.com>
2025-09-17 12:40:28 +0600

    frist c++ program

---
+++ index.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+#include<conio.h>
+
+int main()
+{ 
+    cout << "Animes barma asdfasdf asdfadsf \n asdf sadf asdf   n is \n sdfsadf  C++" << endl;
+    getch();
+}
+ 
